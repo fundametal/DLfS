038c786171715d8ace9c083a209b46338fccb867
fundametal <irenaeus@irenaeus.kr>
2021-04-10 15:58:26 +0900

    ReLu

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
+def relu(x):
+    return np.maximum(0, x)
 
 
 x = np.arange(-5, 6, 0.1)
-y = sigmoid(x)
+y = relu(x)
 
 plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
 plt.show()
